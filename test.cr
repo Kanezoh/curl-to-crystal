3eb3fd612acb260b3c6cfadcd8d3b316d681fc6b
Kanezoh <asfaltmanta114@gmail.com>
2021-04-14 07:42:24 +0900

    use threads in test

--- test.cr
+++ test.cr
@@ -2,84 +2,106 @@ require "spec"
 require "http/server"
 require "duktape/runtime"
 
+REQUESTS = [] of Request
 JS_CONTEXT = Duktape::Runtime.new do |sbx|
   js = File.read("resources/js/curl-to-crystal.js")
   sbx.eval! <<-JS
     #{js}
   JS
 end
-#cmd = "curl http://example.com"
-#JS_CONTEXT.call("curlToCrystal", cmd)
-puts JS_CONTEXT.eval("1 + 1")
 
-#capture_http
-#
-#assert_curl_eq "/"
-
-
-def capture_http
+# server thread
+spawn do
   server = HTTP::Server.new([TestHandler.new]){}
   puts "Listening on http://127.0.0.1:4000"
   server.listen(4000)
 end
 
-def assert_curl_eq(path, curl_args="")
-  curl_req = system "curl -s -o /dev/null http://127.0.0.1:4000#{path} #{curl_args}"
+# test thread
+spawn do
+  sleep 3
+  assert_curl_eq "/"
+  assert_curl_eq "/foo.txt"
+  assert_curl_eq "/", "-X POST -d 'foo'"
+  assert_curl_eq "/", "-X POST -d 'foobar'"
+  assert_curl_eq "/", "-XPOST -H MyCrazyHeader:foo -d 'foo'"
+  assert_curl_eq "/", "-u banana:coconuts"
+  assert_curl_eq "/", <<-CURL
+     -X POST \
+     -u API_KEY: \
+     -d 'shipment[to_address][id]=adr_HrBKVA85' \
+     -d 'shipment[from_address][id]=adr_VtuTOj7o' \
+     -d 'shipment[parcel][id]=prcl_WDv2VzHp' \
+     -d 'shipment[is_return]=true' \
+     -d 'shipment[customs_info][id]=cstinfo_bl5sE20Y'
+     CURL
+  assert_curl_eq "/", "-d foo=bar"
+  assert_curl_eq "/", "-d @README.md"
+  assert_curl_eq "/curl-to-crystal/?foo=bar", "-d @README.md"
+end
+sleep 50
 
-  crystal_req = crystal = curl_to_crystal("curl http://127.0.0.1:4000#{path} #{curl_args}")
-  #assert_equal curl_req.verb, crystal_req.
-  #assert_equal curl_req.path, crystal_req.path
-  #if curl_req.body.nil?
-  #  assert_nil crystal_req.body
-  #else
-  #  assert_equal curl_req.body, crystal_req.body
-  #end
-  #assert_equal curl_req.headers, crystal_req.headers
+def assert_curl_eq(path, curl_args="")
+  system "curl -s -o /dev/null http://127.0.0.1:4000#{path} #{curl_args}"
+  crystal_code = curl_to_crystal("curl http://127.0.0.1:4000#{path} #{curl_args}").to_s
+  crystal_code= crystal_code.gsub("HTTP::Client.new(uri.host.not_nil!)", "HTTP::Client.new(uri.host.not_nil!, port: 4000)" )
+  system "crystal eval '#{crystal_code}'"
+  curl_req = REQUESTS.shift
+  crystal_req = REQUESTS.shift
+  it "method" { curl_req.method.should eq crystal_req.method }
+  it "path" { curl_req.path.should eq crystal_req.path }
+  if curl_req.body == ""
+    it "body should be blank" { crystal_req.body.should eq "" }
+  else
+    it "body" { curl_req.body.should eq crystal_req.body }
+  end
+  it "headers" { curl_req.headers.should eq crystal_req.headers }
 end
 
 def curl_to_crystal(cmd)
   JS_CONTEXT.call("curlToCrystal.default", cmd)
 end
 
-  struct Request
-    property method, path, headers, body
-  
-    def initialize(@method : String, @path : String, @headers : HTTP::Headers, @body : String)
-    end
-  end
-  
-  class TestHandler
-    include HTTP::Handler
-  
-    def call(context)
-      puts (normalize_request context.request)
-    end
+struct Request
+  property method, path, headers, body
 
-    private def normalize_request(original)
-      headers = original.headers.dup
-      headers.delete("accept-encoding")
-      headers.delete("user-agent")
-      headers.delete("host")
-      headers.delete("expect")
-      headers.delete("connection")
-      headers["accept"] = ["*/*"] if headers["accept"].empty? # curl set this as default, but crystal's http/client doesn't do so.
-      body = original.body.to_s
-      if body
-        if headers["content-type"] == ["application/x-www-form-urlencoded"]
-          # May encode slightly differently
-          begin
-            body = URI.decode_www_form(string: body)#.sort
-            headers.delete("content-length")
-          rescue ArgumentError
-          end
+  def initialize(@method : String, @path : String, @headers : HTTP::Headers, @body : String)
+  end
+end
+  
+class TestHandler
+  include HTTP::Handler
+
+  def call(context)
+    request = (normalize_request context.request)
+    REQUESTS.push(request)
+  end
+
+  private def normalize_request(original)
+    headers = original.headers.dup
+    headers.delete("accept-encoding")
+    headers.delete("user-agent")
+    headers.delete("host")
+    headers.delete("expect")
+    headers.delete("connection")
+    headers["accept"] = ("*/*") unless headers.has_key?("accept") # curl set */* as default, but crystal's http/client doesn't do so.
+    body = original.body.to_s
+    if body != ""
+      body =  original.body.not_nil!.gets_to_end
+      if headers["content-type"] == ["application/x-www-form-urlencoded"]
+        # May encode slightly differently
+        begin
+          body = URI.decode_www_form(string: body)#.sort
+          headers.delete("content-length")
+        rescue ArgumentError
         end
       end
-  
-      Request.new(
-        method:  original.method,
-        path:    original.path,
-        headers: headers,
-        body:    body
-      )
     end
+    Request.new(
+      method:  original.method,
+      path:    original.path,
+      headers: headers,
+      body:    body
+    )
   end
+end
